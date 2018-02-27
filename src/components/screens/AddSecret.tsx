836caf7e6beb85111b0fea584ef28a3e63203635
bmartin <barry.martin@move.com>
2018-02-27 15:45:37 -0500

    Being fixing add/edit secret page

--- src/components/screens/AddSecret.tsx
+++ src/components/screens/AddSecret.tsx
@@ -1,10 +1,10 @@
 import * as QueryString from "query-string";
 import * as React from "react";
-import {Config} from "../../config";
-import {PassVaultModel, Secret} from "../../models/passvault";
-import {changeWindowLocation, copyToClipboardFromId, getLocationHash, openInputTextInNewTab} from "../../utils/browser";
-import {getElementById} from "../../utils/browser";
-import {isBlank} from "../../utils/helpers";
+import { Config } from "../../config";
+import { PassVaultModel, Secret } from "../../models/passvault";
+import { changeWindowLocation, copyToClipboardFromId, getLocationHash, openInputTextInNewTab } from "../../utils/browser";
+import { getElementById } from "../../utils/browser";
+import { isBlank } from "../../utils/helpers";
 import { Logger } from "../../utils/logger";
 import { buildFavoritesPath } from "../../utils/passvault";
 import CancelButton from "../elements/CancelButton";
@@ -97,169 +97,157 @@ export default class AddSecret extends React.Component<ComponentProps, Component
     return (
       <main>
         <div className="page-content">
-        <div className="row">
-      <div className="col s12">
-        <div className="col s2 center-align">
-          <h5>
-            <a href="test.com" className="grey-text">
-              <i className="material-icons prefix">panorama_fish_eye</i>
-            </a>
-          </h5>
-        </div>
-        <div className="col s9 left-align">
-          <h5>
-            <TextInput
-              id="website-name"
-              label=""
-              active={true}
-              validate={true}
-              placeholder="Website.Com"
-              colSize={12}
-              onChangeHandler={this.updateName}
-              value={this.state.name}
-              tabIndex={1}
-            />
-          </h5>
-        </div>
-        <div className="col s1 left-align">
-          <h5>
-            <Hidden when={!this.state.editor}>
-              <i className="material-icons prefix" onClick={this.favoriteSecret}>
-                {this.state.favorite ? "favorite" : "favorite_border"}
-              </i>
-            </Hidden>
-          </h5>
-        </div>
-      </div>
-      <div className="col s12">
-        <TextInput
-          id="website"
-          label=""
-          active={true}
-          validate={true}
-          placeholder="website.com"
-          colSize={10}
-          onChangeHandler={this.updateWebsite}
-          value={this.state.website}
-          tabIndex={2}
-        />
-        <div className="col s1 left-align">
-          <h5>
-            <i className="material-icons small" onClick={() => {
-              openInputTextInNewTab("website");
-            }}>open_in_new</i>
-          </h5>
-        </div>
-      </div>
-      <div className="col s12">
-        <TextInput
-          id="username"
-          label=""
-          active={true}
-          validate={true}
-          placeholder="username"
-          colSize={10}
-          onChangeHandler={this.updateUsername}
-          value={this.state.username}
-          tabIndex={3}
-        />
-        <div className="col s1 left-align">
-          <h5>
-              <i className="material-icons small" onClick={() => {
-                copyToClipboardFromId("username");
-              }}>content_copy</i>
-          </h5>
-        </div>
-      </div>
-      <div className="col s12">
-        <TextInput
-          id="password"
-          type="password"
-          label=""
-          active={true}
-          validate={true}
-          placeholder="****************"
-          colSize={9}
-          onChangeHandler={this.updatePassword}
-          value={this.state.password}
-          tabIndex={4}
-        />
-        <div className="col s1 left-align">
-          <h5>
-            <i className="material-icons small" onClick={this.togglePassword}>remove_red_eye</i>
-          </h5>
-        </div>
-        <div className="col s1 left-align">
-          <h5>
-            <i className="material-icons small" onClick={() => {
-                copyToClipboardFromId("password");
-              }}>content_copy</i>
-          </h5>
-        </div>
-      </div>
-      <div className="col s12">
-        <TextInput
-          id="folder"
-          label=""
-          active={true}
-          validate={true}
-          placeholder="folder-name"
-          colSize={10}
-          onChangeHandler={this.updateFolder}
-          value={this.state.folder}
-          tabIndex={5}
-        />
-        {/* TODO : This should be a select box instead of the free text field above */}
-        {/* <select>
+          <div className="row">
+            <div className="col s12">
+              <TextInput
+                id="website-name"
+                label=""
+                active={true}
+                validate={true}
+                placeholder="Website.Com"
+                colSize={10}
+                onChangeHandler={this.updateName}
+                value={this.state.name}
+                tabIndex={1}
+              />
+              <div className="col s1">
+                <Hidden when={!this.state.editor}>
+                  <a className="btn-floating waves-effect waves-light btn-small">
+                    <i className="material-icons prefix" onClick={this.favoriteSecret}>
+                      {this.state.favorite ? "favorite" : "favorite_border"}
+                    </i>
+                  </a>
+                </Hidden>
+              </div>
+            </div>
+            <div className="col s12">
+              <TextInput
+                id="website"
+                label=""
+                active={true}
+                validate={true}
+                placeholder="website.com"
+                colSize={10}
+                onChangeHandler={this.updateWebsite}
+                value={this.state.website}
+                tabIndex={2}
+              />
+              <div className="col s1 min-40">
+                <a className="btn-floating waves-effect waves-light btn-small">
+                  <i className="material-icons" title="Open Website" onClick={() => {
+                    openInputTextInNewTab("website");
+                  }}>open_in_new</i>
+                </a>
+              </div>
+            </div>
+            <div className="col s12">
+              <TextInput
+                id="username"
+                label=""
+                active={true}
+                validate={true}
+                placeholder="username"
+                colSize={10}
+                onChangeHandler={this.updateUsername}
+                value={this.state.username}
+                tabIndex={3}
+              />
+              <div className="col s1 min-40">
+                <a className="btn-floating waves-effect waves-light btn-small">
+                  <i className="material-icons" title="Copy Password" onClick={() => {
+                    copyToClipboardFromId("username");
+                  }}>content_copy</i>
+                </a>
+              </div>
+            </div>
+            <div className="col s12">
+              <TextInput
+                id="password"
+                type="password"
+                label=""
+                active={true}
+                validate={true}
+                placeholder="****************"
+                colSize={9}
+                onChangeHandler={this.updatePassword}
+                value={this.state.password}
+                tabIndex={4}
+              />
+              <div className="col s1 min-40">
+                <a className="btn-floating waves-effect waves-light btn-small">
+                  <i className="material-icons" title="Show/Hide Password" onClick={this.togglePassword}>remove_red_eye</i>
+                </a>
+              </div>
+              <div className="col s1 min-40">
+                <a className="btn-floating waves-effect waves-light btn-small">
+                  <i className="material-icons" title="Copy Password" onClick={() => {
+                    copyToClipboardFromId("password");
+                  }}>content_copy</i>
+                </a>
+              </div>
+            </div>
+            <div className="col s12">
+              <TextInput
+                id="folder"
+                label=""
+                active={true}
+                validate={true}
+                placeholder="folder-name"
+                colSize={10}
+                onChangeHandler={this.updateFolder}
+                value={this.state.folder}
+                tabIndex={5}
+              />
+              {/* TODO : This should be a select box instead of the free text field above */}
+              {/* <select>
           <option value="" disabled selected>Select Folder</option>
           <option value="1">Default</option>
           <option value="2">Business</option>
           <option value="3">Entertainment</option>
           <option value="4" data-icon="plus-circle-outline.png" className="left">Add new....</option>
         </select> */}
-        <div className="col s1">
-          <h5>
-            <i className="material-icons small">folder</i>
-          </h5>
-        </div>
-      </div>
-      <form className="col s11 offset-s1">
-        <div className="row">
-          <TextArea
-            id="notes"
-            onChangeHandler={this.updateNotes}
-            value={this.state.notes}
-            label="Notes"
-            colSize={12}
-          />
-        </div>
-      </form>
-      <div className="col s12 center-align">
-        <div className="col s1 offset-s1">
-          <div className="valign-wrapper">
-            <h5>
-              <Hidden when={!this.state.editor}>
-                <i className="material-icons small" onClick={this.deleteSecret}>delete</i>
-              </Hidden>
-            </h5>
+              <div className="col s1 min-40">
+                <a className="btn-floating waves-effect waves-light btn-small">
+                  <i className="material-icons small">folder</i>
+                </a>
+              </div>
+            </div>
+            <form className="col s12">
+              <div className="row">
+                <TextArea
+                  id="notes"
+                  onChangeHandler={this.updateNotes}
+                  value={this.state.notes}
+                  label="Notes"
+                  colSize={12}
+                />
+              </div>
+            </form>
+            <div className="col s12 center-align">
+              <div className="col s2">
+                <div className="valign-wrapper">
+                  <Hidden when={!this.state.editor}>
+                    <a className="btn-floating waves-effect waves-light">
+                      <i className="material-icons" title="Delete Secret" onClick={this.deleteSecret}>delete</i>
+                    </a>
+                  </Hidden>
+                </div>
+              </div>
+              <div className="col s5">
+                {/* TODO: What do we do on a cancel event */}
+                <CancelButton
+                  onclickHandler={() => { changeWindowLocation(Config.PAGE_MAIN); }}
+                />
+              </div>
+              <div className="col s5">
+                <ConfirmButton
+                  text="Save"
+                  onclickHandler={this.saveSecret}
+                  disable={!this.state.modified}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="col s5">
-          {/* TODO: What do we do on a cancel event */}
-          <CancelButton
-            onclickHandler={() => {changeWindowLocation(Config.PAGE_MAIN); }}
-          />
-        </div>
-        <div className="col s5">
-          <ConfirmButton
-            text="Save"
-            onclickHandler={this.saveSecret}
-            disable={!this.state.modified}
-          />
-        </div>
-
-      </div>
-    </div>
         </div>
       </main>
     );
