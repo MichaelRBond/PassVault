52c7b6008dc2259f38d987d66b99d28f15608783
Michael Bond <MichaelRBond@users.noreply.github.com>
2018-02-27 16:48:38 -0500

    Add button to main page on password generation page
    1. Add Main Page button
    1. fix spacing issues (4 space mixed with 2 space tabs)
    1. made methods that didn't need to be public private

--- src/components/screens/PasswordGenerator.tsx
+++ src/components/screens/PasswordGenerator.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
+import {Config} from "../../config";
+import {changeWindowLocation} from "../../utils/browser";
 import {generatePassword} from "../../utils/crypto";
 import Checkbox from "../elements/Checkbox";
+import ConfirmButton from "../elements/ConfirmButton";
 import RoundButton from "../elements/RoundButton";
 import TextInput from "../elements/TextInput";
 
@@ -32,53 +35,6 @@ export default class PasswordGenerator extends React.Component<{}, ComponentStat
     };
   }
 
-  public updatePassword() {
-      this.setState({
-          ...this.state,
-          generatedPassword: generatePassword(this.state.passwordLength, {
-              lowercase: this.state.enableLowercase,
-              capitals: this.state.enableCapitals,
-              numbers: this.state.enableNumbers,
-              symbols: this.state.enableSymbols,
-          }),
-      });
-  }
-
-  public setEnableCaptials(enableCapitals: boolean) {
-    this.setState({
-        ...this.state,
-        enableCapitals,
-    });
-  }
-
-  public setEnableLowercase(enableLowercase: boolean) {
-    this.setState({
-        ...this.state,
-        enableLowercase,
-    });
-  }
-
-  public setEnableNumbers(enableNumbers: boolean) {
-    this.setState({
-        ...this.state,
-        enableNumbers,
-    });
-  }
-
-  public setEnableSymbols(enableSymbols: boolean) {
-    this.setState({
-        ...this.state,
-        enableSymbols,
-    });
-  }
-
-  public setPasswordLength(length: number) {
-      this.setState({
-          ...this.state,
-          passwordLength: length,
-      });
-  }
-
   public render() {
     return (
       <main>
@@ -88,61 +44,115 @@ export default class PasswordGenerator extends React.Component<{}, ComponentStat
               <img src={logo} />
             </div>
             <div className="row">
-                <div className="col s12 center-align pad-top-20">
-                  <TextInput
-                      id="generatedPassword"
-                      validate={false}
-                      colSize={10}
-                      value={this.state.generatedPassword}
-                    />
-                    <RoundButton onclickHandler={() => this.updatePassword()} margin={20}/>
-                </div>
-                <div className="col s12 center-align">
-                    <TextInput
-                      id="passwordLength"
-                      label="Length"
-                      type="number"
-                      validate={true}
-                      colSize={12}
-                      value={this.state.passwordLength}
-                      active={true}
-                      onChangeHandler={(e: any) => this.setPasswordLength(parseInt(e.currentTarget.value, 10))}
-                    />
-                </div>
-                    <div className="col s12">
-                        <p>
-                            <Checkbox
-                                label="A-Z"
-                                onChangeHandler={(enabled: boolean) => this.setEnableCaptials(enabled)}
-                                checked={this.state.enableCapitals}
-                            />
-                        </p>
-                        <p>
-                            <Checkbox
-                                label="a-z"
-                                onChangeHandler={(enabled: boolean) => this.setEnableLowercase(enabled)}
-                                checked={this.state.enableLowercase}
-                            />
-                        </p>
-                        <p>
-                            <Checkbox
-                                label="0-9"
-                                onChangeHandler={(enabled: boolean) => this.setEnableNumbers(enabled)}
-                                checked={this.state.enableNumbers}
-                            />
-                        </p>
-                        <p>
-                            <Checkbox
-                                label="!@#$%&amp;"
-                                onChangeHandler={(enabled: boolean) => this.setEnableSymbols(enabled)}
-                                checked={this.state.enableSymbols}
-                            />
-                        </p>
-                    </div>
+              <div className="col s12 center-align pad-top-20">
+                <TextInput
+                  id="generatedPassword"
+                  validate={false}
+                  colSize={10}
+                  value={this.state.generatedPassword}
+                />
+                <RoundButton onclickHandler={() => this.updatePassword()} margin={20}/>
+              </div>
+              <div className="col s12 center-align">
+                <TextInput
+                  id="passwordLength"
+                  label="Length"
+                  type="number"
+                  validate={true}
+                  colSize={12}
+                  value={this.state.passwordLength}
+                  active={true}
+                  onChangeHandler={(e: any) => this.setPasswordLength(parseInt(e.currentTarget.value, 10))}
+                />
+              </div>
+              <div className="col s12">
+                <p>
+                  <Checkbox
+                    label="A-Z"
+                    onChangeHandler={(enabled: boolean) => this.setEnableCaptials(enabled)}
+                    checked={this.state.enableCapitals}
+                  />
+                </p>
+                <p>
+                  <Checkbox
+                    label="a-z"
+                    onChangeHandler={(enabled: boolean) => this.setEnableLowercase(enabled)}
+                    checked={this.state.enableLowercase}
+                  />
+                </p>
+                <p>
+                  <Checkbox
+                    label="0-9"
+                    onChangeHandler={(enabled: boolean) => this.setEnableNumbers(enabled)}
+                    checked={this.state.enableNumbers}
+                  />
+                </p>
+                <p>
+                  <Checkbox
+                    label="!@#$%&amp;"
+                    onChangeHandler={(enabled: boolean) => this.setEnableSymbols(enabled)}
+                    checked={this.state.enableSymbols}
+                  />
+                </p>
+              </div>
             </div>
+          </div>
+          <div className="col center-align pad-top-50">
+            <ConfirmButton
+              text="Main Page"
+              onclickHandler={() => changeWindowLocation(Config.PAGE_MAIN)}
+              type=""
+            />
           </div>
         </div>
       </main>
     );
+  }
+
+  private updatePassword() {
+    this.setState({
+      ...this.state,
+      generatedPassword: generatePassword(this.state.passwordLength, {
+        lowercase: this.state.enableLowercase,
+        capitals: this.state.enableCapitals,
+        numbers: this.state.enableNumbers,
+        symbols: this.state.enableSymbols,
+      }),
+    });
+  }
+
+  private setEnableCaptials(enableCapitals: boolean) {
+    this.setState({
+      ...this.state,
+      enableCapitals,
+    });
+  }
+
+  private setEnableLowercase(enableLowercase: boolean) {
+    this.setState({
+      ...this.state,
+      enableLowercase,
+    });
+  }
+
+  private setEnableNumbers(enableNumbers: boolean) {
+    this.setState({
+      ...this.state,
+      enableNumbers,
+    });
+  }
+
+  private setEnableSymbols(enableSymbols: boolean) {
+    this.setState({
+      ...this.state,
+      enableSymbols,
+    });
+  }
+
+  private setPasswordLength(length: number) {
+    this.setState({
+      ...this.state,
+      passwordLength: length,
+    });
   }
 }
